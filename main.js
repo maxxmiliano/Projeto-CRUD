76ec669ba4101bf68b39ee04cf16dbb37a54a4da
unknown <maxmilianossouza@hotmail.com>
2023-03-29 21:28:24 -0300

    corrigindo erro linha 126 e 143

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const openModal = () => document.getElementById('modal')
   .classList.add('active')
 
  const closeModal = () =>{
-  document.getElementById('modal').classList.remove('active')
   clearFields()
+  document.getElementById('modal').classList.remove('active')
+  
  } 
    
 
@@ -17,7 +18,19 @@ const openModal = () => document.getElementById('modal')
  const setLocalstorage = (dbClient) => localStorage.setItem("db_client" , JSON.stringify(dbClient))
   //inicio dos eventos
 
- 
+  const deleteClient = (index) => {
+    const dbClient = readClient()
+    dbClient.splice(index, 1)
+    setLocalstorage(dbClient)
+   }
+
+   const updateClient = (index, client) => {
+    const dbClient = readClient()
+    dbClient[index] = client
+    setLocalstorage(dbClient)
+   }
+    
+   const readClient = () => getLocalstorage()
 
  const createClient = (client) => { //create
    const dbClient = getLocalstorage()
@@ -25,19 +38,11 @@ const openModal = () => document.getElementById('modal')
    setLocalstorage(dbClient)
  }
 
- const updateClient = (index, client) => {
-  const dbClient = readClient()
-  dbClient[index] = client
-  setLocalstorage(dbClient)
- }
 
- const deleteClient = (index) => {
-  const dbClient = readClient()
-  dbClient.splice(index, 1)
-  setLocalstorage(dbClient)
- }
 
- const readClient = () => getLocalstorage()
+ 
+
+ 
  
  const isValidFields = () => {
     return document.getElementById('form').reportValidity()
@@ -53,7 +58,7 @@ const openModal = () => document.getElementById('modal')
  const saveClient = () => {
   if (isValidFields()) {
     const client = {
-      nome:document.getElementById('nome').value,
+      nome: document.getElementById('nome').value,
       email: document.getElementById('email').value,
       celular: document.getElementById('celular').value,
       cep: document.getElementById('cep').value
@@ -89,20 +94,16 @@ const createRow = (client, index) => {
           <button type="button" class="button red" id="delete-${index}" >excluir</button>
        </td>
   `
-    document.querySelector('#tableC>tbody').appendChild(newRow)
+    document.querySelector('#tableClient>tbody').appendChild(newRow)
 }
 
+  
 
-   const editClient = (index) => {
-      const client = readClient()[index]
-      client.index = index
-      fillFields(client)
-      document.querySelector(".modal-header>h2").textContent = `editando ${client.nome}`
-      openModal()
-   }
+
+  
 
    const clearTable = () => {
-    const rows = document.querySelectorAll('#tableC tr')
+    const rows = document.querySelectorAll('#tableClient>tbody tr')
     rows.forEach(row => row.parentNode.removeChild(row))
   }
 
@@ -112,13 +113,21 @@ const createRow = (client, index) => {
    dbClient.forEach(createRow)
 }
 
-   const fillFields = (client) => {
-    document.getElementById('nome').value = client.nome
-    document.getElementById('email').value = client.email
-    document.getElementById('celular').value = client.celular
-    document.getElementById('cep').value = client.cep
-    document.getElementById('nome').dataset.index = client.index
-   }
+const fillFields = (client) => {
+  document.getElementById('nome').value = client.nome
+  document.getElementById('email').value = client.email
+  document.getElementById('celular').value = client.celular
+  document.getElementById('cep').value = client.cep
+  document.getElementById('nome').dataset.index = client.index
+}
+
+  const editClient = (index) => {
+    const client = readClient()[index]
+     client.index = index
+     fillFields(client)
+     document.querySelector('.modal-header>h2').textContent = `editando ${client.nome}`
+     openModal()
+}
 
  const editDelete = (event) => {
 
@@ -126,7 +135,7 @@ const createRow = (client, index) => {
 
           const [action, index] = event.target.id.split('-')
 
-         if (action == 'editar') {
+         if (action == 'edit') {
           editClient(index)
          }
          else{
@@ -147,19 +156,17 @@ const createRow = (client, index) => {
 
 updateTable()
 
+    document.getElementById('cadastrocliente') 
+      .addEventListener('click', openModal)
 
-  document.querySelector('#tableC>tbody')
-  .addEventListener('click', editDelete)
+    document.getElementById('modalClose') 
+       .addEventListener('click', closeModal) 
 
-  document.getElementById('cadastrocliente') 
-  .addEventListener('click', openModal)
-
-  document.getElementById('modalClose') 
-  .addEventListener('click', closeModal) 
- 
-
-  document.getElementById('salvar')
-  .addEventListener('click', saveClient)
-
+   document.getElementById('salvar')
+       .addEventListener('click', saveClient)
+  
+  document.querySelector('#tableClient>tbody')
+      .addEventListener('click', editDelete)
+      
   document.getElementById('cancelar')
     .addEventListener('click', closeModal)
